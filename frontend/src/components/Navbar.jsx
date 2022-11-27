24bebcc8ca054ca42fc21393f2cf4477fa1b8d2c
emreeken183425 <emreeken1834@gmail.com>
2022-11-27 23:35:33 +0300

    klasör

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
       <div className="container-fluid  ">
     <a className="navbar-brand" href="#">CASE STUDY</a>
       <form className="d-flex  justify-content-center " role="search">
-        <input className="form-control m-1 w-100   " type="search" placeholder="Search a book ..."  />
+        <input className="form-control m-1 w-100   " type="search" placeholder="Search a Book ..."  />
         <button class="btn btn-outline btn-success" type="submit">Search</button>
       </form>
       <button class="btn btn-outline btn-warning" type="submit">Add new record</button>
