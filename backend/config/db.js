2e13da89b066c5884886ae6b29e1ef0152055a32
emreeken183425 <emreeken1834@gmail.com>
2022-11-28 20:08:51 +0300

    again

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import pg from 'pg'
-import dotenv from 'dotenv'
-
-dotenv.config()
-const postgresClient=new pg.Pool({
-    connectionString:process.env.DB_CONNECTİON_STRİNG
-})
-
-export default postgresClient
